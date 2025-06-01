1170e12110feac7fee62612cfc4609ab5a6ce441
Amani Khalid <amanikhalidd@gmail.com>
2025-06-01 08:30:07 +0400

    Inserted Data Into Students Table

--- TrainingDB.sql
+++ TrainingDB.sql
@@ -12,3 +12,10 @@ CREATE TABLE Students (
     EnrollmentDate DATE
 );
 
+
+--Insert Data into Students table
+INSERT INTO Students VALUES  
+(1, 'Sara Ali', '2023-09-01'), 
+(2, 'Mohammed Nasser', '2023-10-15');
+
+
