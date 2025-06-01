639eb981b754f9217f4ea3217495b15307d54145
Amani Khalid <amanikhalidd@gmail.com>
2025-06-01 09:14:27 +0400

    Created Departments Table

--- HospitalDB.sql
+++ HospitalDB.sql
@@ -2,5 +2,11 @@
 
 CREATE DATABASE HospitalDB;
 
+USE HospitalDB;
 
+-- Create Departments table
+CREATE TABLE Departments (
+    DepartmentID INT PRIMARY KEY IDENTITY(1,1),
+    DepartmentName NVARCHAR(100) NOT NULL
+);
    
