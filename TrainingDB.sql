cbf5113a713eb0d21e8bb58c07aa4d977812f75b
Amani Khalid <amanikhalidd@gmail.com>
2025-06-01 08:27:12 +0400

    Created Students Table

--- TrainingDB.sql
+++ TrainingDB.sql
@@ -4,3 +4,11 @@ CREATE DATABASE TrainingDB;
 
 USE TrainingDB;
 
+-- Create Students Table
+
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    FullName NVARCHAR(100),
+    EnrollmentDate DATE
+);
+
