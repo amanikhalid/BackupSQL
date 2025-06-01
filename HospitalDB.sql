46c5288379dc0eda2d73ae0ee027745c91b34c94
Amani Khalid <amanikhalidd@gmail.com>
2025-06-01 09:15:34 +0400

    Created Doctors Table

--- HospitalDB.sql
+++ HospitalDB.sql
@@ -10,3 +10,12 @@ CREATE TABLE Departments (
     DepartmentName NVARCHAR(100) NOT NULL
 );
    
+-- Create Doctors table
+CREATE TABLE Doctors (
+    DoctorID INT PRIMARY KEY IDENTITY(1,1),
+    FirstName NVARCHAR(50),
+    LastName NVARCHAR(50),
+    DepartmentID INT,
+    FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID)
+);
+
